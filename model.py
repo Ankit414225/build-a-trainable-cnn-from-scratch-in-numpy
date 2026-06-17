9d2566ac2272e08d8bfb81f2c5fcdae6d77971d2
Ankit414225 <ankit784910@gmail.com>
2026-06-18 01:51:18 +0530

    Step 10: he_std

--- model.py
+++ model.py
@@ -57,8 +57,10 @@ def accuracy(logits_or_probs, labels):
     predicted=argmax_rows(logits_or_probs)
     return np.mean((predicted==labels))
 
-# Step 10 - he_std (not yet solved)
-# TODO: implement
+# Step 10 - he_std
+def he_std(fan_in):
+    # TODO: return the He initialization standard deviation sqrt(2 / fan_in).
+    return np.sqrt(2.0/fan_in)
 
 # Step 11 - he_init (not yet solved)
 # TODO: implement
